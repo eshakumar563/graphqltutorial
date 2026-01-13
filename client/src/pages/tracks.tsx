ebf33de2b9396ff7de7e653a0308ce345dda01c5
ekumar <ekumar@paloaltonetworks.com>
2026-01-13 14:10:39 -0800

    Renders the page

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Layout, QueryResult } from "../components";
-import { gql } from "../__generated__";
-import { useQuery } from "@apollo/client";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 import TrackCard from "../containers/track-card";
- 
+
 /** TRACKS query to retrieve all tracks */
-const TRACKS = gql(`
+const TRACKS = gql`
   query GetTracks {
     tracksForHome {
       id
@@ -20,23 +20,24 @@ const TRACKS = gql(`
       }
     }
   }
-`);
+`;
+
 /**
-* Tracks Page is the Catstronauts home page.
-* We display a grid of tracks fetched with useQuery with the TRACKS query
-*/
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
-   
+
   return (
     <Layout grid>
       <QueryResult error={error} loading={loading} data={data}>
-        {data?.tracksForHome?.map((track) => (
+        {(data as any)?.tracksForHome?.map((track: any) => (
           <TrackCard key={track.id} track={track} />
         ))}
       </QueryResult>
     </Layout>
   );
 };
- 
+
 export default Tracks;
