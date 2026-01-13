b645430bc1550e536362d29c88297e6afffd5fa7
ekumar <ekumar@paloaltonetworks.com>
2026-01-13 12:05:35 -0800

    client updated for running

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const TRACKS = gql(`
+query ExampleQuery {
+  tracksForHome {
+    id
+    length
+    modulesCount
+    thumbnail
+    title
+    author {
+      id
+      name
+      photo
+    }
+  }
+}
+`);
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return <Layout grid>{JSON.stringify(data)} </Layout>;
 };
 
 export default Tracks;
