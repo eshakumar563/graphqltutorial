711faee4e386a23ecf0fa91a4c93b970c5282b87
ekumar <ekumar@paloaltonetworks.com>
2026-01-13 14:06:03 -0800

    Adding tracks page to the homepage

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 /** importing our pages */
 import Tracks from "./tracks";
 
+
 export default function Pages() {
   return (
     <BrowserRouter>
